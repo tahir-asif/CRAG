16b412eb344c3f4d047eb380cea1df97be7412d9
Tahir Asif <tahir.asif@proton.me>
2026-09-15 15:21:30 -0400

    Feat: add exception wrapper to translate at the boundry

---
+++ app/exceptions.py
@@ -0,0 +1,6 @@
+class LLMError(Exception):
+    """Raised when LLM generation fails. Carries an HTTP status code."""
+
+    def __init__(self, message: str, status_code: int = 502):
+        super().__init__(message)
+        self.status_code = status_code
